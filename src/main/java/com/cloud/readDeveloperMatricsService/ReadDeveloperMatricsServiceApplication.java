e016fb994262c9856d9491bbfab3fc24b67b2766
hasini <164124V@uom.lk>
2023-12-06 15:49:45 +0530

    initial commit

---
+++ src/main/java/com/cloud/readDeveloperMatricsService/ReadDeveloperMatricsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.readDeveloperMatricsService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadDeveloperMatricsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadDeveloperMatricsServiceApplication.class, args);
+	}
+
+}
